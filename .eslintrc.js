bfdfc872d4fe72dc715069d431879408ff1a2731
Javier López <lopez.pardo@gmail.com>
2022-04-12 18:47:44 +0200

    fix lint issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,12 @@
 module.exports = {
-  extends: [
-    'next/core-web-vitals',
-    '@cabify/eslint-config/recommended',
-    'plugin:typescript-sort-keys/recommended',
-  ],
-  plugins: ['typescript-sort-keys', 'sort-keys-fix'],
+  extends: ['next/core-web-vitals', 'plugin:typescript-sort-keys/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     createDefaultProgram: true,
     project: './tsconfig.eslint.json',
     tsconfigRootDir: __dirname,
   },
+  plugins: ['typescript-sort-keys', 'sort-keys-fix'],
   rules: {
     'import/no-default-export': 'off',
     'sort-keys-fix/sort-keys-fix': 'error',
