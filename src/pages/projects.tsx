df6d334d9f4c03a5cb51aa9aa64c4111598cc699
Javier López <lopez.pardo@gmail.com>
2022-05-12 17:54:13 -0700

    add proper projects section

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,58 +1,30 @@
-import { SectionTitle } from '../components/SectionTitle';
+import { GetStaticPropsContext, InferGetStaticPropsType } from 'next';
 
-export default function Projects() {
+import { ProjectCard } from '../components/ProjectCard';
+import { SectionTitle } from '../components/SectionTitle';
+import projectsJson from '../data/projects.json';
+
+export const getStaticProps = (_ctx: GetStaticPropsContext) => {
+  return {
+    props: {
+      projects: projectsJson,
+    },
+  };
+};
+
+export default function Projects({
+  projects,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <>
-      {' '}
-      <section>
-        <SectionTitle>Projects</SectionTitle>
-        <ul className="mt-1">
-          <li className="py-2">
-            <div className="flex justify-between my-1">
-              <strong>Lorem Ipsum</strong>
-              <p className="flex">
-                <span className="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">
-                  HTML
-                </span>
-                <span className="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">
-                  CSS
-                </span>
-                <span className="bg-gray-600 text-white px-2 py-1 ml-1 text-xs rounded">
-                  JS
-                </span>
-              </p>
-            </div>
-            <ul className="flex mb-2">
-              <li>
-                <a
-                  className="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded"
-                  href="#"
-                >
-                  Live
-                </a>
-              </li>
-              <li>
-                <a
-                  className="bg-blue-600 text-white px-2 py-1 mr-1 text-sm rounded"
-                  href="#"
-                >
-                  Code
-                </a>
-              </li>
-            </ul>
-            <p className="text-xs">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-              delectus labore enim in minus quod vero dignissimos et, ratione
-              obcaecati quis maiores? Voluptatem, natus cupiditate perferendis
-              omnis ex hic incidunt! Earum dolore cupiditate sed et maxime
-              distinctio iure fugiat aspernatur at veniam laudantium eveniet
-              corporis dicta reiciendis quod consequatur, labore perferendis
-              dolorum velit quibusdam minus iste dolorem! Officiis, obcaecati
-              maxime
-            </p>
-          </li>
-        </ul>
-      </section>
-    </>
+    <section>
+      <SectionTitle>Projects</SectionTitle>
+      <ul className="mt-1">
+        {projects.map(({ description, ...project }) => (
+          <ProjectCard key={project.title} {...project}>
+            {description}
+          </ProjectCard>
+        ))}
+      </ul>
+    </section>
   );
 }
